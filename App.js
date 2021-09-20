fe93196dc5bf4aaa0d3b022c30de541593089348
Guilherme-dsr <guilhermesilva.dsr@gmail.com>
2021-09-19 21:53:48 -0300

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,86 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+    StyleSheet, 
+    Text, 
+    TouchableOpacity, 
+    ImageBackground,
+    ScrollView, 
+    View 
+} from 'react-native';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={styles.container}>
+      <View style={styles.header}>
+        <Text> Voltar </Text>
+        <Text> Titulo </Text>
+        <Text> Perfil </Text>
+      </View>
+      <View style={styles.postContainer}>
+        <Text style={styles.postTitle}> Titulo </Text>
+        <Text style={styles.postDescription}> A expressão Lorem ipsum em design gráfico... </Text>
+      </View>
+      <View style={styles.postContainer}>
+        <Text style={styles.postTitle}> Titulo </Text>
+        <Text style={styles.postDescription}> A expressão Lorem ipsum em design gráfico... </Text>
+      </View>
+      <View style={styles.postContainer}>
+        <Text style={styles.postTitle}> Titulo </Text>
+        <Text style={styles.postDescription}> A expressão Lorem ipsum em design gráfico... </Text>
+      </View>
+      <View style={styles.postContainer}>
+        <Text style={styles.postTitle}> Titulo </Text>
+        <Text style={styles.postDescription}> A expressão Lorem ipsum em design gráfico... </Text>
+      </View>
+      <View style={styles.postContainer}>
+        <Text style={styles.postTitle}> Titulo </Text>
+        <Text style={styles.postDescription}> A expressão Lorem ipsum em design gráfico... </Text>
+      </View>
+      <View style={styles.postContainer}>
+        <Text style={styles.postTitle}> Titulo </Text>
+        <Text style={styles.postDescription}> A expressão Lorem ipsum em design gráfico... </Text>
+      </View>
+      <View style={styles.postContainer}>
+        <Text style={styles.postTitle}> Titulo </Text>
+        <Text style={styles.postDescription}> A expressão Lorem ipsum em design gráfico... </Text>
+      </View>
+
+    </ScrollView> 
+     
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex:1,
+    backgroundColor:'#333',
+    paddingTop:40,
+  },
+  header: {
+    height:70,
+    backgroundColor:'#fff',
+    paddingHorizontal:30,
+    paddingTop:20,
+    justifyContent:'space-between',
+    alignItems:'center',
+    flexDirection:'row',
+  },
+  postContainer: {
+    marginHorizontal:20,
+    marginVertical:30,
+    padding:20,
+    backgroundColor:'#fff',
+    borderRadius:3,
+  },
+  postTitle: {
+    fontSize:20,
+    fontWeight:'bold',
+    marginBottom:20,
+  },
+  postDescription: {
+    color:'#666'
   },
 });
+
